816cb2aab96b6c909a5ce1fc164300d33192ff27
CoolBenWork <ben1@yandex.ru>
2023-02-13 15:03:19 +0300

    edit Example005_ContitionifElse

---
+++ Example005_ContitionifElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write = ("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username == "маша")
+
+{
+	Console.WriteLine("Ура, это же Маша!");
+}
+
+else
+
+{
+	Console.Write("Привет, ");
+	Console.WriteLine(username);
+}
