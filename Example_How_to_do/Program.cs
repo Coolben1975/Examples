257818adc9bcfa5ac2c4bb84210b0a80f9b4f650
CoolBenWork <ben1@yandex.ru>
2023-03-23 11:32:55 +0300

    Add Exemple How to do

--- Example_How_to_do/Program.cs
+++ Example_How_to_do/Program.cs
@@ -1,2 +1,16 @@
 ﻿using System.Linq;
 
+string text = "(1,2) (2,3) (3,4) (4,5)"
+				.Replace("(", "")
+				.Replace(")", "")
+				;
+var data = text.Split(" ")
+				.Select(item => item.Split(","))
+				.Select(e => (int.Parse(e[0]), int.Parse(e[1])))
+				.ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+	Console.WriteLine(data[i]);
+	Console.WriteLine();
+}
